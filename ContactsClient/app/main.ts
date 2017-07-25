c96e598916e73120eeb994a0ddc53c2e8d8f160a
Carolyn <carolynlschroeder@hotmail.com>
2017-07-25 10:18:11 -0500

    Reorganized ContactClient project

--- ContactsClient/app/main.ts
+++ ContactsClient/app/main.ts
@@ -1,4 +1,4 @@
 // main entry point
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { AppModule } from './app.module';
 platformBrowserDynamic().bootstrapModule(AppModule);
